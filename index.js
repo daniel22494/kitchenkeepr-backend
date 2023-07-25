c4d45216fe16497d5526fc18d0709c464e9d16e4
Daniel_Martinez <daniel.umanamartinez@gmail.com>
2023-07-25 15:52:33 -0400

    extracted caption from url

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+// const express = require("express");
+// const cors = require("cors");
+const axios = require("axios");
+const { parse } = require("node-html-parser");
 
-const app = express();
-const PORT = process.env.PORT || 5050;
+// const app = express();
+// app.use(cors());
 
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
+// app.get("/", async (req, res) => {
+//   const { url } = req.query;
+//   const caption = await getCaption(url);
+//   res.json({ caption });
+// });
 
-app.listen(PORT, () => {
-  console.log("server started on port", PORT);
+// app.listen(8080, () => {
+//   console.log("ok!");
+// });
+
+async function getCaption(url) {
+  const { data } = await axios.get(url);
+  const root = parse(data);
+  const desc = root.querySelector("meta[property='og:title']");
+  const content = desc.getAttribute("content");
+  const i = content.indexOf(":");
+  return content.slice(i + 2);
+}
+
+// getCaption("https://www.instagram.com/p/B_UC-cQJtXG/").then((caption) => {
+//   console.log(caption);
+// });
+
+getCaption("https://www.instagram.com/reel/CjxmT0oJkJH/").then((caption) => {
+  console.log(caption);
 });
