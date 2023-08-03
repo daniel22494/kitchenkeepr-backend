8468053123e14fc6594183848f540faa5349013e
Daniel_Martinez <daniel.umanamartinez@gmail.com>
2023-08-03 12:17:32 -0400

    added new seeds

--- seeds/04_favourites.js
+++ seeds/04_favourites.js
@@ -6,9 +6,9 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("favourites").del();
   await knex("favourites").insert([
-    { id: 1, recipe_id: 10 },
-    { id: 2, recipe_id: 12 },
-    { id: 3, recipe_id: 13 },
-    { id: 4, recipe_id: 14 },
+    { id: 1, recipe_id: 3 },
+    { id: 2, recipe_id: 6 },
+    { id: 3, recipe_id: 7 },
+    { id: 4, recipe_id: 8 },
   ]);
 };
